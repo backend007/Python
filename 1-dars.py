9c331ee5cdb0c5e77f0d461756687ac2812884bd
Python and PHP backend <130725441+backend007@users.noreply.github.com>
2023-06-03 23:12:43 +0500

    Create 1-dars.py
    23:12 da ozgartirildi

---
+++ 1-dars.py
@@ -0,0 +1,11 @@
+menu = ['osh','qazonkabob','shashlik','norin','somsa']
+buyurtmalar = ["osh","somsa","manti", "shashlik"]
+
+if buyurtmalar: # ro'yxatda biror element bo'lsa bu ifoda TRUE qaytaradi
+    for taom in buyurtmalar:
+        if taom in menu:
+            print(f"Menuda {taom} bor")
+        else:
+            print(f"Kechirasiz, menuda {taom} yo'q")
+else: # agar ro'yxat bo'sh bo'lsa
+    print("Savatchangiz bo'sh!")
